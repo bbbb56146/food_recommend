3dc437297763463e2b488ae5318e07bb592d31ff
Seyeon Kim <sydsh19@naver.com>
2021-05-30 23:08:49 +0900

    Initial Commit

---
+++ food_recommend/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  food_recommend
+//
+//  Created by 김세연 on 2021/05/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
