585f320a7e33ff945bd2231362e2ff2f4d5d0f5e
Seyeon Kim <sydsh19@naver.com>
2021-06-20 08:57:11 +0900

    for pull

---
+++ request.swift
@@ -0,0 +1,14 @@
+//
+//  GetJSON.swift
+//  food_recommend
+//
+//  Created by 김세연 on 2021/06/20.
+//
+
+import SwiftUI
+
+var PARAM = "food_freq={\"닭갈비\": 6, \"오일파스타\": 8, \"김밥\": 3, \"된장찌개\": 5, \"쌀국수\": 2}&user_feedback={\"닭갈비\": 4, \"김밥\": 13, \"떡볶이\": 7}"
+var IPaddr = "http://52.79.168.220:5000//method?"
+
+var 
+
